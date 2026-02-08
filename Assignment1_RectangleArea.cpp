28fb9448fa63b1e89cb180a0545ddd7795fe4528
Cuahutemoc Vazquez <vazquez.cuah@gmail.com>
2026-02-08 00:47:54 -0800

    2/8

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -10,15 +10,18 @@ int main()
 	// get height and width from user
 	double height;
 	double width;
-	cout << "Enter height and width: ";
+	cout << "Enter a height: ";
 /* write your code here */
+cin>>height;
+cout << "Enter a width: ";
+cin>>width;
 	// calculate area
-	
+	double area;
+	area=height*width;
 
 	// write output to console
-	
+	cout<<"the area of the rectangle with a height of "<<height <<" and a width of "<<width<<" is "<<area<<" squared units"<<endl;
 
 	// return value that indicates normal program exit
     return 0;
 }
-
