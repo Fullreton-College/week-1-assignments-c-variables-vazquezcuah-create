4df6da47ab8db5f011017628d760c9d53b81f8f4
Cuahutemoc Vazquez <vazquez.cuah@gmail.com>
2026-02-08 21:05:14 -0800

    Update Assignment2_PaceConverter.cpp

--- Assignment2_PaceConverter.cpp
+++ Assignment2_PaceConverter.cpp
@@ -6,11 +6,22 @@ int main()
     double mph = 6.5;
     // PACE calculation
     /* write your code */
-
+cout<<"enter a speed: "<<endl;
+cin>>mph;
+double time;
+time=60/mph;
+int mins;
+mins=static_cast<int>(time);
+double secs;
+secs=time-mins;
+secs=secs*600;
+int intsecs=static_cast<int>(secs);
+secs=static_cast<double>(intsecs)/10;
+cout<<"With a speed of "<<mph<< " miles per hour, you will complete a mile in "<<mins<<" minutes and "<<secs<< " seconds"<<endl;
     //express pace in minutes and seconds
   /* write your code here */
 
   //express your output in 1 decimal points
-
+return 0;
 
 }
